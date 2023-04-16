40c77817cb4460fb84111b2acbc81385729ddfcd
farzan-alaei <farzan.70@gmail.com>
2023-04-16 19:28:51 +0430

    complete the todo app
    add todo name to url in template
    edit import in urls.py in todo app
    add forms for add and edit task
    redesign views import in urls.py of todo app
    adding view for task list , create , update , delete and complete task

--- core/todo/forms.py
+++ core/todo/forms.py
@@ -1,1 +1,18 @@
-from django.forms import forms
+from django import forms
+from .models import Task
+
+
+class TaskForm(forms.ModelForm):
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control rounded-4",
+                "name": "title",
+                "placeholder": "enter the title",
+            }
+        ),
+        label="",
+    )
+    class Meta:
+        model = Task
+        fields = ('title',)
--- core/todo/urls.py
+++ core/todo/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
+from . import views
 app_name = 'todo'
 
 
 urlpatterns = [
-    
+    path('', views.list_view, name='task-list'),
+    path('update/<int:pk>/', views.update_task, name='update-task'),
+    path('complete/<int:pk>/', views.complete_task, name='complete-task'),
+    path('delete/<int:pk>/', views.delete_task, name='delete-task'),
 ]
